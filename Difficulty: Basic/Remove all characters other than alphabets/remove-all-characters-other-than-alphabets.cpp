de04b0300fb866c6c286ce8071ddbc73f3057b13
KhushiSai <87372782+KhushiSai@users.noreply.github.com>
2024-09-19 11:22:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove all characters other than alphabets/remove-all-characters-other-than-alphabets.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    string removeSpecialCharacter(string s) {
+        // code here
+        string result;
+        for(char c :s){
+            if(isalpha(c)){
+                result+=c;
+            }
+          
+        }
+        return result.empty()?"-1" :result;
+       
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Solution ob;
+        cout << ob.removeSpecialCharacter(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
