cca195d871b614e82f58cf2dd7bd6d495458dd04
KhushiSai <87372782+KhushiSai@users.noreply.github.com>
2024-12-06 11:22:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (6.56%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
         int n =nums.size();
-        vector<int>output(n , 0);
-        int product =1;
-        int zero_count=0;
-       
-        
+        vector<int>ans(n);
+        vector<int>prefix(n);
+        vector<int>postfix(n);
+        prefix[0]=1;
+        for(int i=1;i<n ;i++){
+            prefix[i]=nums[i-1]*prefix[i-1];
+        }
+        postfix[n-1]=1;
+        for(int i=n-2 ;i>=0 ;i--){
+            postfix[i]=nums[i+1]*postfix[i+1];
+        }
         for(int i=0;i<n ;i++){
-            if(nums[i]==0){
-                zero_count++;
-            }
-            else{
-                 product=product*nums[i];
+            ans[i]=prefix[i]*postfix[i];
+        }
+        return ans ;
 
-            }
-           
-        }
-         if(zero_count>1){
-            return output;
-         }
-         if (zero_count == 1) {
-            for (int i = 0; i < n; i++) {
-                if (nums[i] == 0) {
-                    output[i] = product; // Only the element corresponding to the zero gets the product
-                }
-            }
-            return output;
-        }
-         
-        for(int i=0;i<n; i++){
-            output[i]=product/nums[i];
-        }
-        return output;
         
     }
 };
