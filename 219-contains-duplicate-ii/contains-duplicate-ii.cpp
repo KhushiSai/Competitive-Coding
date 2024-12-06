6d68bd6e8819327a3253520430d34154185288fe
KhushiSai <87372782+KhushiSai@users.noreply.github.com>
2024-12-06 10:11:35 +0530

    Time: 1591 ms (11.59%) | Memory: 64 MB (87.21%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n=nums.size();
+        int i=0 ;
+        while(i<n){
+            int j=i+1;
+            while(j<n &&j<=i+k){
+            if(nums[i]==nums[j] ){
+                return true ;
+        
+            }
+            j++;
+            }
+            i++;
+        }
+
+      return false;
+        
+    }
+};
